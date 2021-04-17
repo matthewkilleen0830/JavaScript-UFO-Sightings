3b790a6acaaecd6543aac2afe712da9178978f19
matthewkilleen0830 <matthew.killeen0830@gmail.com>
2021-04-17 10:50:34 -0500

    Added level 2 index.html and app.js.  Will deploy level 2 index.html to GitHub pages after grading.

--- ufo-level-2/static/js/app.js
+++ ufo-level-2/static/js/app.js
@@ -1,6 +1,9 @@
 // Declare variable to store object in data.js
 var tableData = data;
 
+// Reference variable declared in buildTable function and use d3 to display it on index.html
+var tableBody = d3.select("tbody");
+
 // Define function to create table from data.js file
 function buildTable(sightings) {
     tableBody.html("");
@@ -29,45 +32,55 @@ function buildTable(sightings) {
     });
 };
 
+// Declare variable to store empty dictionary
+var filterDictionary = {};
+
+// Define function to filter on multiple criteria
+function multipleFilters() {
+
+    // Prevent page from reloading using D3
+    d3.event.preventDefault();
+
+    // Declare variables to store inputs
+    var inputCriteria = d3.select(this).select("input");
+    var inputValue = inputCriteria.property("value");
+    var filteredID = inputCriteria.attr("id");
+
+    // Conditional statement to call filterTable function and populate array
+    if (inputValue) {
+        filterDictionary[filteredID] = inputValue;      
+    }
+    else {
+        delete filterDictionary[filteredID];     
+    }
+
+    // Call filterTable function
+    filterTable();
+}
+
 // Define function to filter table by dates input by user
 function filterTable() {
 
     // Prevent page from reloading using D3
     d3.event.preventDefault();
 
-    // Declare variable to store input date
-    var inputDate = d3.select("#datetime");
-    var inputValue = inputDate.property("value");
-
     // Declare variable to store a copy of data for filtering
     var filteredData = tableData;
 
-    // Conditional statement to reset original table on button click
-    if (inputValue != "") {
-        var filteredData = tableData.filter(function(sightingRow) {
-            
-            // Boolean conditional statement to display table rows matching input date
-            if (inputValue === sightingRow.datetime) {
-                return true;
-            }
-        });
-    };
+    // forEach statement to retrieve multiple values from table
+    Object.entries(filterDictionary).forEach(([objectKey, objectValue]) => {
+        filteredData = filteredData.filter(tableRow => tableRow[objectKey] === objectValue);
+    });
+
+    // Call buildTable function
     buildTable(filteredData);
 };
 
-// Reference variable declared in buildTable function and use d3 to display it on index.html
-var tableBody = d3.select("tbody");
+// Declare variables to reference "Filter Table" button
+var filterButton = d3.selectAll(".filter");
+
+// Create event handler to call filterTable function when "Filter Table" button is clicked
+filterButton.on("change", multipleFilters);
 
 // Call buildTable function
 buildTable(tableData);
-
-// Declare variables to reference "Filter Table" button and input form
-var filterButton = d3.select("#filter-btn");
-var formEnter = d3.select("#datetime");
-
-// Create event handlers to call filterTable function when "Filter Table" button is clicked or "enter" is pressed
-filterButton.on("click", filterTable);
-formEnter.on("submit", filterTable);
-
-// <input type = "submit"> somewhere in the form line in index.html???
-// formEnter event handler not calling filterTable function, it actually refreshes the page
